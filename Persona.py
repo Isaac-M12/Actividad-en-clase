c3835a6cf30eb10432037c12bc0a549a55c88c4b
Isaac-M12 <112027561+Isaac-M12@users.noreply.github.com>
2022-12-06 22:41:25 -0500

    Add files via upload

--- Persona.py
+++ Persona.py
@@ -1,17 +1,17 @@
 class Persona:
     nombre        = str
     edad          = int
-    centroEstudio = str
-    def __init__(self,nombre,edad, centroEstudio):
+    CentroEstudio = str
+    def __init__(self,nombre,edad, CentroEstudio):
         self.nombre        = nombre 
         self.edad          = edad
-        self.centroEstudio = centroEstudio
+        self.CentroEstudio = CentroEstudio
         
     def conversar (self,otra_persona):
-        return f'Hola {otra_persona.nombre} me llamo {self.nombre}, y estudio en la  {self.centroEstudio} '  
+        return f'Hola {otra_persona.nombre} me llamo {self.nombre} tengo 18 años, estudio en el {self.CentroEstudio}'  
     
 if __name__== "__main__" :
-    Persona1 = Persona("Isaac", 18, "ISTYAVIRAC")
+    Persona1 = Persona("Isaac", 18, "Instituto YAVIRAC y estoy en el segundo semestre de la carrera Desarrollo de Software")
     Persona2 = Persona("Vanessa", 18, "U.Central") 
     
     print(Persona1.conversar(Persona2))  
